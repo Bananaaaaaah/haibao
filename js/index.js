f3049f3801c1e8ebd4452d7c64ed6a9e4168ca7b
Bananaaaaaah <871433259@qq.com>
2018-02-27 17:46:28 +0800

    update

--- js/index.js
+++ js/index.js
@@ -3,27 +3,169 @@
  */
 
 window.onload = function () {
+
+
+    //slide1
+    var bannerSlide = document.getElementById("banner-slide");
+    var slide1 = document.getElementsByClassName("slide1");
+    for (i=0;i<slide1.length;i++){
+
+    slide(slide1[i]);
+
+    }
+    //点击下标切换
+    //为所有btnSpan绑定单击响应
+    //var index = 0;
+
+   function slide(name) {
+       var ul = name.getElementsByTagName("ul")[0];
+       var li =ul.getElementsByTagName("li");
+
+       var prev = name.getElementsByClassName("nav-prev")[0];
+       var next = name.getElementsByClassName("nav-next")[0];
+
+       var slideBtns =name.getElementsByClassName("slide-btns")[0];
+       if(slideBtns){
+           var btnSpan = slideBtns.getElementsByTagName("span");
+           for(var i=0;i<btnSpan.length;i++){
+               btnSpan[i].index = i;
+               btnSpan[i].onclick = function () {
+                   clearInterval(timer)
+                   //排他
+                   index = this.index
+                   console.log(index)
+                   setBtns();
+                   //第一张 0 -750
+                   //第二张 1 -1500
+                   //第三张 2 -2250
+
+                   // ul.style.left = PAGE_WIDTH *index + PAGE_WIDTH +"px";
+                   move(ul,'left',PAGE_WIDTH *index + PAGE_WIDTH,30,function () {
+                       //   //  atuoSlide();
+                   })
+               }
+
+           }
+       }
+
+       var PAGE_WIDTH = -parseInt(getStyle(li[0],'width'));
+       var index = 0;
+
+       function setBtns() {
+           //判断当前索引是否是最后一张
+           if(index >= li.length-2){
+               //将当前索引设置为第一张的索引
+               index = 0;
+               ul.style.left = PAGE_WIDTH+"px";
+           }
+           if(index <0){
+               index =li.length-3;
+               ul.style.left =PAGE_WIDTH *(li.length-2) +"px"
+           }
+           for(var i=0;i<btnSpan.length;i++){
+               btnSpan[i].className = "";
+           }
+           btnSpan[index].className = "on";
+       }
+
+
+       //读取元素样式
+       function getStyle(obj,name) {
+           //获取元素当前显示样式  currentStyle只有IE支持 getComputedStyle其他浏览器
+           if(window.getComputedStyle){
+               return getComputedStyle(obj,null)[name];
+           }else {
+               return obj.currentStyle[name];
+           }
+       }
+       //定时器执行动画效果
+       function move(obj,attr,target,speed,callback) {
+           clearInterval(obj.timer);
+           //获取当前位置
+           var  current =parseInt(getStyle(obj, attr));
+           //判断速度正负
+           speed = current<target? speed:-speed;
+           obj.timer = setInterval(function () {
+               var oldValue = parseInt(getStyle(obj, attr))
+               var newValue = oldValue + speed;
+               console.log(oldValue)
+               console.log(current)
+               //向左移动newValue是否<target
+               //向右移动newValue是否>target
+               if (speed < 0 && newValue < target || speed > 0 && newValue > target) {
+                   newValue = target;
+               }
+               obj.style[attr] = newValue + "px";
+               //移动到目标位置停止
+               if (newValue == target) {
+                   //关闭定时器
+                   clearInterval(obj.timer);
+                   callback && callback();
+               }
+
+           },30)
+
+
+       }
+       //点击前后切换
+
+       next.onclick = function () {
+           index++
+           index%=(li.length-1)
+           move(ul,'left',PAGE_WIDTH*(index+1),50,function () {
+               setBtns();
+           });
+
+       }
+       prev.onclick = function () {
+           index--
+           index %=(li.length-1);
+           move(ul,'left',PAGE_WIDTH*(index+1),50,function () {
+               setBtns();
+           });
+
+       }
+       var timer;
+       //自动切换
+       function atuoSlide() {
+           timer =  setInterval(function () {
+               index++;
+               //判断index的值
+               index %=(li.length-1);
+               //执行move
+               move(ul,'left',PAGE_WIDTH*(index+1),30,function () {
+                   setBtns();
+               });
+           },3000)
+       }
+      // atuoSlide();
+   }
+
+
+
+
+
+
     //搜索框
     var navSearch = document.getElementsByClassName("nav-search")[0];
     var searchNav = document.getElementsByClassName("search-nav-wrap")[0];
+    var count = 1;
+    function open(a,b) {
+        if(count%2 == 1){
+            a.className += " open"
+            b.style.display="block";
+        }
+        if(count%2 == 0){
 
-    var i = 1
-    console.log(i)
+            a.className="nav-search fr";
+            b.style.display="none";
+        }
+        count++;
+    }
     navSearch.onclick = function () {
-        if(i%2 == 1){
-            navSearch.className += " open"
-            searchNav.style.display="block";
-        }
-        if(i%2 == 0){
-
-            navSearch.className="nav-search fr";
-            searchNav.style.display="none";
-        }
-        i++;
+        open(navSearch,searchNav)
 
     }
-
-
     //精品计划tab切换
     var plan = document.getElementsByClassName("exclusive-plan");
     var sub_nav = document.getElementsByClassName("sub-nav");
@@ -60,38 +202,53 @@ window.onload = function () {
     var header = document.getElementById("header");
     var rightNav = document.getElementById("right-fixed-nav");
     var t=0;
-    var time;
-   window.onscroll= function() {
-       //清除Timeout，
-       clearTimeout(time);
-       //获取滚动条滚动过的距离
-       var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+
+    function scroll(obj,obj2){
+        //清除Timeout，
+        clearTimeout(obj.timer);
+        //获取滚动条滚动过的距离
+        var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
         if(scrollTop == 0){
-            header.style.top = '0';
+            obj.style.top = '0';
         }
         //scrollTop >= t时，说明鼠标向下滚动，
         else if(scrollTop >= t){
-            header.style.top = '-90px';
+            obj.style.top = '-90px';
             //当滚动到一定位置时右侧导航栏弹出
             if(scrollTop >= 800){
-                rightNav.style.display ='block';
+                obj2.style.display ='block';
             }
         }else{   //scrollTop >= t时，说明鼠标向上滚动，
-            header.style.top = '0';
+            obj.style.top = '0';
             //当滚动到一定位置时右侧导航栏消失
             if(scrollTop < 800){
-                rightNav.style.display ='none';
+                obj2.style.display ='none';
             }
             //3s后关闭导航
-            time = setTimeout(function(){
-                header.style.top = '-90px';
+            obj.timer = setTimeout(function(){
+                obj.style.top = '-90px';
             },3000);
         }
         //将当前的scrollTop赋值给t，之后每一次鼠标滚动，只要比当前scrollTop小说明向上滚动，比当前scrollTop大说明向下滚动
-       t =scrollTop;
+        t =scrollTop;
+    }
+   window.onscroll= function() {
+        scroll(header,rightNav)
    }
 
-
+  //回到顶部
+    var goTop = document.getElementsByClassName("gotop")[0];
+    goTop.onclick = function () {
+        //平滑滚动
+      var timer=  setInterval(function () {
+            var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+            document.body.scrollTop  = scrollTop - 30;
+            if (document.body.scrollTop == 0 ) {
+                //关闭定时器
+                clearInterval(timer);
+            }
+        },0)
+    }
 }
 
 
